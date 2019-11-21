97d65643dbffd655dc89f01dc331d15b7af9eeef
qamarhuma <56897085+qamarhuma@users.noreply.github.com>
2019-11-21 14:24:14 -0500

    Add files via upload
    Revised model using opioid gram/person as the exposure rather than grams opioid.

--- Data Analysis/OpioidMortality_GTWR analysis.R
+++ Data Analysis/OpioidMortality_GTWR analysis.R
@@ -26,6 +26,7 @@ opioid_df$age_clean <- ifelse((abs(opioid_df$median_age_moe) < opioid_df$median_
 opioid_df$white_clean <- ifelse((abs(opioid_df$white_alone_moe) < opioid_df$white_alone*0.15), opioid_df$white_alone, NA)
 opioid_df$percent_white <- opioid_df$white_clean/opioid_df$pop_clean * 100
 opioid_df$mortality_rate <- opioid_df$deaths_clean/opioid_df$pop_clean * 100000
+opioid_df$opioid_perperson <- opioid_df$opioids_in_gm/opioid_df$pop_clean
 
 # log transform mortality rate
 opioid_df$log_mortality <- log(opioid_df$mortality_rate)
@@ -41,6 +42,8 @@ hist(opioid_df$opioids_in_gm)
 hist(opioid_df$log_mortality)
 boxplot(opioid_df$opioids_in_gm)
 boxplot(opioid_df$opioids_in_gm~opioid_df$year)
+boxplot(opioid_df$opioid_perperson)
+boxplot(opioid_df$opioid_perperson~opioid_df$year)
 boxplot(opioid_df$deaths_clean~opioid_df$year)
 boxplot(opioid_df$mortality_rate~opioid_df$year)
 boxplot(opioid_df$log_mortality~opioid_df$year) # increases over time
@@ -48,9 +51,15 @@ boxplot(opioid_df$age_clean)
 boxplot(opioid_df$hhincome_clean)
 boxplot(opioid_df$percent_white)
 boxplot(opioid_df$pop_clean)
-scatter.smooth(opioid_df$opioids_in_gm, opioid_df$deaths_clean) 
+
+# Explore the global linear regression to determine the global model
 global_association <- lm(log_mortality ~ opioids_in_gm + hhincome_clean + age_clean + pop_clean + percent_white, data = opioid_df)
 summary (global_association)
+plot(global_association, which = 1)
+
+global_association_pp <- lm(log_mortality ~ opioid_perperson + hhincome_clean + age_clean + percent_white, data = opioid_df)
+summary(global_association_pp)
+plot(global_association_pp, which = 1)
 
 ## merge with lat/long data
 opioid_df$fips <- opioid_df$geoid
@@ -62,11 +71,11 @@ merged_data <- merge(county_centers, opioid_df, by="fips")
 
 #subset the data and convert to spatial points dataframe
 varlist <- c("fips", "name.x", "state.x", "county", "state.y", "year", "longitude", "latitude", 
-             "deaths_clean", "mortality_rate", "log_mortality", "opioids_in_gm", "pop_clean", 
+             "deaths_clean", "mortality_rate", "log_mortality", "opioids_in_gm", "opioid_perperson",
              "hhincome_clean", "age_clean", "white_clean", "percent_white" )
 analysis_data <- subset(merged_data, select = varlist)
 analysis_data <- subset(analysis_data, state.y!=15)
-analysis_data <- subset(analysis_data, !(is.na(deaths_clean)) & !(is.na(pop_clean)) & !(is.na(hhincome_clean)))
+analysis_data <- subset(analysis_data, !(is.na(deaths_clean)) & !(is.na(hhincome_clean)))
 coordinates(analysis_data) <- ~longitude + latitude
 aea.proj <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-102
               +x_0=180 +y_0=50 +ellps=GRS80 +datum=NAD83 +units=m"
@@ -78,17 +87,17 @@ class(analysis_data)
 
 #(a) identify bandwidth with fixed kernel (fixed distance, but varying number of nearest neighbours)
 
-fixedbisquare_bw <- bw.gtwr(log_mortality ~ opioids_in_gm + hhincome_clean + age_clean + pop_clean + percent_white, data=analysis_data,
+fixedbisquare_bw <- bw.gtwr(log_mortality ~ opioid_perperson + hhincome_clean + age_clean + percent_white, data=analysis_data,
                      obs.tv = analysis_data$year, approach = "AICc", kernel = "bisquare", adaptive = F, lamda = 0.05, t.units = "year", ksi = 0,
                       verbose = TRUE)
 
-fixedgaussian_bw <- bw.gtwr(log_mortality ~ opioids_in_gm + hhincome_clean + age_clean + pop_clean + percent_white, data=analysis_data,
+fixedgaussian_bw <- bw.gtwr(log_mortality ~ opioid_perperson + hhincome_clean + age_clean + percent_white, data=analysis_data,
                      obs.tv = analysis_data$year, approach = "AICc", kernel = "gaussian", adaptive = F, lamda = 0.05, t.units = "year", ksi = 0,
                      verbose = TRUE)
 
-## lowest AIC for the bandwidth obtained using fixed gaussian kernel
+## lowest AIC for badnwidth selection obtained using fixed gaussian kernel
 #Run the GTWR model with the fixed gaussian bandwidth
-gtwr_model <- gtwr(log_mortality ~ opioids_in_gm + hhincome_clean + age_clean + pop_clean + percent_white, data=analysis_data, 
+gtwr_model <- gtwr(log_mortality ~ opioid_perperson + hhincome_clean + age_clean + percent_white, data=analysis_data, 
                     obs.tv = analysis_data$year, st.bw = fixedgaussian_bw, kernel="gaussian",
                     adaptive=FALSE, p=2, theta=0, longlat=F,lamda=0.05,t.units = "year",ksi=0)
 
@@ -96,15 +105,15 @@ gtwr_model ## summary of model
 
 ## add the coefficients to the analysis dataset
 
-results <- as.data.frame(gtwr_model1$SDF)
+results <- as.data.frame(gtwr_model$SDF)
 head(results)
 analysis_data$coef_intercept <- results$Intercept
-analysis_data$coef_opioids_in_gm <- results$opioids_in_gm
+analysis_data$coef_opioids_perperson <- results$opioid_perperson
 analysis_data$coef_hhincome_clean <- results$hhincome_clean
 analysis_data$coef_age_clean <- results$age_clean
 analysis_data$coef_pop_clean <- results$pop_clean
 analysis_data$coef_pop_clean <- results$percent_white
-analysis_data$coef_opioids_per1000_gm <- results$opioids_in_gm*1000 # scaled coefficient to a 1000 gm increase in yearly opiod
+analysis_data$coef_opioids_per100_gm_pp <- results$opioid_perperson*100 # scaled coefficient to a 100 gm increase in yearly opiod
 
 write.csv(analysis_data, "Mortality Outcome GTWR Model Outputs 2006-2012.csv", row.names=FALSE) # output complete case data with coefficients for mapping in D3
 
