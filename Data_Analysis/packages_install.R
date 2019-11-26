9185f64eeae5aad3d5c01ca70f656ed2aa9dc685
Dan Higgins <daniel.higgins@yahoo.com>
2019-11-25 20:08:10 -0600

    added scripts for install and updates to website

---
+++ Data_Analysis/packages_install.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript
+install.packages('GWmodel',dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages('sp',dependencies=TRUE, repos='http://cran.rstudio.com/')
+#install.packages('sgdal',dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages('countyweather',dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages('ggplot2',dependencies=TRUE, repos='http://cran.rstudio.com/')
